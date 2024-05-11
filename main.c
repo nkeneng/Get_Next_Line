97ce777c3e81d1ee6706b0aaafb04f9649369131
steven Nkeneng <snkeneng@student.42berlin.de>
2024-05-11 19:00:45 +0000

    included main.c and bkp progress

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snkeneng <snkeneng@student.42berlin.       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 11:48:29 by snkeneng          #+#    #+#             */
+/*   Updated: 2024/05/11 11:48:34 by snkeneng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	printf("buffer size  is %d\n", BUFFER_SIZE);
+	fd = open("text.txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	close(fd);
+	free(line);
+	return (0);
+}
